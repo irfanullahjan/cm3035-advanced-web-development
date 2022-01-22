7cff5d0d4b007664e466d0fec00ca78fe0566de2
Irfanullah Jan <irfanullah.jan@outlook.com>
2022-01-22 11:17:20 +0500

    improved pfam tests

--- midterm/proteins/tests.py
+++ midterm/proteins/tests.py
@@ -1,7 +1,4 @@
-import json
-from django.test import TestCase
 from django.urls import reverse
-from django.urls import reverse_lazy
 
 from rest_framework.test import APIRequestFactory
 from rest_framework.test import APITestCase
@@ -9,26 +6,37 @@ from rest_framework.test import APITestCase
 from .model_factories import *
 from .serializers import *
 
+
 class PfamTest(APITestCase):
-  def test_pfamDetailOk(self):
-    pfam = PfamFactory()
-    url = reverse('domain-pfam-detail', kwargs={'domain_id': pfam.domain_id})
-    response = self.client.get(url)
-    self.assertEqual(response.status_code, 200)
 
-  def test_pfamDetailNotFound(self):
-    url = reverse('domain-pfam-detail', kwargs={'domain_id': 'notfound'})
-    response = self.client.get(url)
-    self.assertEqual(response.status_code, 404)
+    pfam = None
 
-  def test_pfamDetailDomainId(self):
-    pfam = PfamFactory()
-    url = reverse('domain-pfam-detail', kwargs={'domain_id': pfam.domain_id})
-    response = self.client.get(url)
-    self.assertEqual(response.data['domain_id'], pfam.domain_id)
-  
-  def test_pfamDetailDomainDescription(self):
-    pfam = PfamFactory()
-    url = reverse('domain-pfam-detail', kwargs={'domain_id': pfam.domain_id})
-    response = self.client.get(url)
-    self.assertEqual(response.data['domain_description'], pfam.domain_description)
+    def setUp(self):
+        self.pfam = PfamFactory()
+
+    def tearDown(self):
+        Pfam.objects.all().delete()
+
+    def test_pfamDetailOk(self):
+        url = reverse('domain-pfam-detail',
+                      kwargs={'domain_id': self.pfam.domain_id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_pfamDetailNotFound(self):
+        url = reverse('domain-pfam-detail', kwargs={'domain_id': 'notfound'})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_pfamDetailDomainId(self):
+        url = reverse('domain-pfam-detail',
+                      kwargs={'domain_id': self.pfam.domain_id})
+        response = self.client.get(url)
+        self.assertEqual(response.data['domain_id'], self.pfam.domain_id)
+
+    def test_pfamDetailDomainDescription(self):
+        url = reverse('domain-pfam-detail',
+                      kwargs={'domain_id': self.pfam.domain_id})
+        response = self.client.get(url)
+        self.assertEqual(
+            response.data['domain_description'], self.pfam.domain_description)
