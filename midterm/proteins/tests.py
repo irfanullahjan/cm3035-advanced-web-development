204e6068c786d99910788ea95b22b61234888624
Irfanullah Jan <irfanullah.jan@outlook.com>
2022-01-22 11:12:21 +0500

    More tests for Pfam

--- midterm/proteins/tests.py
+++ midterm/proteins/tests.py
@@ -10,8 +10,25 @@ from .model_factories import *
 from .serializers import *
 
 class PfamTest(APITestCase):
-  def test_pfamDetail(self):
+  def test_pfamDetailOk(self):
     pfam = PfamFactory()
     url = reverse('domain-pfam-detail', kwargs={'domain_id': pfam.domain_id})
     response = self.client.get(url)
     self.assertEqual(response.status_code, 200)
+
+  def test_pfamDetailNotFound(self):
+    url = reverse('domain-pfam-detail', kwargs={'domain_id': 'notfound'})
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 404)
+
+  def test_pfamDetailDomainId(self):
+    pfam = PfamFactory()
+    url = reverse('domain-pfam-detail', kwargs={'domain_id': pfam.domain_id})
+    response = self.client.get(url)
+    self.assertEqual(response.data['domain_id'], pfam.domain_id)
+  
+  def test_pfamDetailDomainDescription(self):
+    pfam = PfamFactory()
+    url = reverse('domain-pfam-detail', kwargs={'domain_id': pfam.domain_id})
+    response = self.client.get(url)
+    self.assertEqual(response.data['domain_description'], pfam.domain_description)
