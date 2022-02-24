65b4d843ac1b2d83e099878d732ae8f24422ddc3
Irfanullah Jan <irfanullah.jan@outlook.com>
2022-02-24 20:18:49 +0500

    basic layout

---
+++ final/frontend/components/Layout.tsx
@@ -0,0 +1,56 @@
+import {
+  Col,
+  Container,
+  Nav,
+  Navbar,
+  NavItem,
+  Row,
+} from 'react-bootstrap';
+import { NavbarTop } from './NavbarTop';
+import Link from 'next/link';
+
+type Props = {
+  children: JSX.Element;
+};
+
+export function Layout(props: Props) {
+  const { children } = props;
+  return (
+    <>
+      <div className="bg-dark">
+        <Container>
+          <Row>
+            <Col>
+              <NavbarTop />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div style={{ flex: '1 0 auto' }}>
+        <Container>
+          <Row>
+            <Col>{children}</Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="bg-dark text-secondary" style={{ flexShrink: 0 }}>
+        <Container>
+          <Row>
+            <Col>
+              <Navbar>
+                <Nav navbar>
+                  <NavItem>
+                    <Link href="/about">About</Link>
+                  </NavItem>
+                </Nav>
+                <span>
+                  &copy; EasyHomes {new Date().getFullYear()}
+                </span>
+              </Navbar>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+}
