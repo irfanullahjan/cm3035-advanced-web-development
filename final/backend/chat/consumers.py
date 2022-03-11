c4905c6a9958c32affbb984ce9bfab58a22dea07
Irfanullah Jan <irfanullah.jan@outlook.com>
2022-03-11 17:12:47 +0500

    public lobby working

--- final/backend/chat/consumers.py
+++ final/backend/chat/consumers.py
@@ -1,22 +1,51 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from datetime import datetime
+from asgiref.sync import async_to_sync
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.room_group_name = 'lobby'
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # Leave lobby
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        print(text_data_json)
         message = text_data_json['message']
         username = text_data_json['username']
+
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username,
+                'timestamp': datetime.now().strftime('%H:%M hrs')
+            }
+        )
+
+    def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+        timestamp = event['timestamp']
+
+        # Send message to WebSocket
         self.send(text_data=json.dumps({
             'message': message,
             'username': username,
-            'timestamp': datetime.now().strftime('%H:%M hrs')
+            'timestamp': timestamp
         }))
